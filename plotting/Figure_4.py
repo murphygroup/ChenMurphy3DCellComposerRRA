899b7ccfb262c2f5ec6e97a94ac7914de5666211
hrch3n <chr_3742@hotmail.com>
2024-08-03 12:49:20 -0500

    revision update

--- plotting/Figure_4.py
+++ plotting/Figure_4.py
@@ -9,108 +9,93 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import matplotlib.legend as mlegend
 import pandas as pd
-import sys
 
 if __name__ == '__main__':
-	channel_list = ['actin','lysosome','golgi','tublin','mito']
-	channel_list_vis = ['Actin filaments','Lysosome','Golgi apparatus','Microtubule','Mitochondria']
-	pca_dir = '.data/PCA_model'
-	cmap = ['deepskyblue', 'darkred', 'darkgoldenrod', 'darkkhaki', 'darkslateblue', 'darksalmon', 'chocolate', 'darkgoldenrod', 'darkcyan', 'darkgrey']
+	channel_list = ['actin', 'lysosome', 'golgi', 'tublin', 'mito']
+	channel_list_vis = ['Actin filaments', 'Lysosome', 'Golgi apparatus', 'Microtubule', 'Mitochondria']
+	pca_dir = './data/PCA_model'
+	cmap = ['deepskyblue', 'darkred', 'darkgoldenrod', 'darkkhaki', 'darkslateblue', 'darksalmon', 'chocolate',
+	        'darkcyan', 'darkgrey']
 	marker = ["o", "s", "^", "P", "D", "*", "X", "h", "v", "d", "p"]
 	methods = ['deepcell_membrane-0.12.6', 'cellpose-2.2.2', 'aics_ml']
 	methods_vis = ['3DCellComposer w/ DeepCell', '3DCellComposer w/ Cellpose', 'ACSS']
 	noise_list = ['original']
-	ss, pca = pickle.load(open(join(pca_dir, 'pca_AICS.pkl'), 'rb'))
+	
+	ss, pca = pickle.load(open(join(pca_dir, 'pca_ACSS.pkl'), 'rb'))
 	pc1_explained = "{:.0f}%".format(pca.explained_variance_ratio_[0] * 100)
 	pc2_explained = "{:.0f}%".format(pca.explained_variance_ratio_[1] * 100)
 	fig, ax = plt.subplots()
 	
 	weighted_score_list = []
 	for channel in channel_list:
-		data_dir = f'./data/AICS/AICS_{channel}'
-
+		data_dir = f'/data/3D/AICS/AICS_{channel}'
 		weighted_score_channel_list = []
+		
 		for method in methods:
-			avg_method_metrics_pca_pieces = list()
-			avg_method_metrics_pieces = list()
+			avg_method_metrics_pca_pieces = []
 			for noise in noise_list:
 				if method != 'aics_ml':
-					metrics_dir_list = list()
+					metrics_dir_list = []
 					img_dir_list = sorted(glob.glob(f'{data_dir}/**/original', recursive=True))
 					for img_dir in img_dir_list:
 						optimal_JI = np.load(f'{img_dir}/metrics/optimal_JI_{method}.npy')
-						print(optimal_JI)
-						metrics_dir_list.append(f'{os.path.dirname(img_dir)}/{noise}/metrics/metrics_{method}_{optimal_JI}.npy')
+						metrics_dir_list.append(
+							f'{os.path.dirname(img_dir)}/{noise}/metrics/metrics_{method}_{optimal_JI}.npy')
 				else:
-					metrics_dir_list = sorted(glob.glob(f'{data_dir}/**/{noise}/**/metrics_{method}_0.0.npy', recursive=True))
-	
-				current_metrics_pieces = list()
-				for metrics_dir in metrics_dir_list:
-					current_metrics_pieces.append(np.load(metrics_dir))
+					metrics_dir_list = sorted(
+						glob.glob(f'{data_dir}/**/{noise}/**/metrics_{method}_0.0.npy', recursive=True))
+				
+				current_metrics_pieces = [np.load(metrics_dir) for metrics_dir in metrics_dir_list]
 				current_metrics = np.vstack(current_metrics_pieces)
 				
 				current_metrics = np.delete(current_metrics, 5, axis=1)
-
-
+				
 				current_metrics_z = ss.transform(current_metrics)
-				avg_method_metrics_pieces.append(np.average(current_metrics_z))
 				current_metrics_pca = pca.transform(current_metrics_z)
-
-
+				
 				avg_current_metrics_pca = np.average(current_metrics_pca, axis=0)
-	
 				avg_method_metrics_pca_pieces.append(avg_current_metrics_pca)
 				
 				if noise == 'original':
-					weighted_score = np.exp(sum(avg_current_metrics_pca[i] * pca.explained_variance_ratio_[i] for i in range(2)))
+					weighted_score = np.exp(
+						sum(avg_current_metrics_pca[i] * pca.explained_variance_ratio_[i] for i in range(2)))
 					weighted_score_channel_list.append(weighted_score)
-	
+			
 			avg_method_metrics_pca = np.vstack(avg_method_metrics_pca_pieces)
 			method_idx = methods.index(method)
 			channel_idx = channel_list.index(channel)
+			
 			ax.plot(avg_method_metrics_pca[:, 0], avg_method_metrics_pca[:, 1], color=cmap[method_idx])
-			ax.scatter(avg_method_metrics_pca[:, 0], avg_method_metrics_pca[:, 1], edgecolors=cmap[method_idx], s=20, facecolors='none', marker = marker[channel_idx])
-			ax.scatter(avg_method_metrics_pca[0, 0], avg_method_metrics_pca[0, 1], edgecolors='black', s=50, facecolors=cmap[method_idx], marker = marker[channel_idx])
+			ax.scatter(avg_method_metrics_pca[:, 0], avg_method_metrics_pca[:, 1], edgecolors=cmap[method_idx], s=20,
+			           facecolors='none', marker=marker[channel_idx])
+			ax.scatter(avg_method_metrics_pca[0, 0], avg_method_metrics_pca[0, 1], edgecolors='black', s=50,
+			           facecolors=cmap[method_idx], marker=marker[channel_idx])
+		
 		weighted_score_list.append(weighted_score_channel_list)
-
+	
 	plt.xlabel(f'PC1({pc1_explained})')
 	plt.ylabel(f'PC2({pc2_explained})')
 	
 	color_handles = [patches.Patch(color=color, label=methods_vis[i]) for i, color in
 	                 enumerate(cmap[:len(methods_vis)])]
+	marker_handles = [plt.Line2D([0], [0], marker=marker, color='k', linestyle='', label=channel_list_vis[i]) for
+	                  i, marker in enumerate(marker[:len(channel_list_vis)])]
 	
-	marker_handles = [plt.Line2D([0], [0], marker=marker, color='k', linestyle='', label=channel_list_vis[i]) for i, marker
-	                  in enumerate(marker[:len(channel_list_vis)])]
-	
-	# Add the legend to the plot
 	plt.xlim([3.58, 4.25])
-	# Create a legend object
 	upper_left_legend = mlegend.Legend(ax, handles=color_handles, labels=methods_vis, loc='upper left')
-	
-	# Add the legend to the plot
 	ax.add_artist(upper_left_legend)
-	
-	# Create a second legend object
 	lower_right_legend = mlegend.Legend(ax, handles=marker_handles, labels=channel_list_vis, loc='lower right')
-	
-	# Add the second legend to the plot
 	ax.add_artist(lower_right_legend)
 	plt.tight_layout()
-
-	plt.savefig(f'{os.path.dirname(pca_dir)}/fig/AICS_original_PCA.png', dpi=500)
+	plt.savefig(f'{os.path.dirname(pca_dir)}/fig/ACSS_original_PCA.png', dpi=500)
 	plt.clf()
-	
-	
 	
 	weighted_score_list = np.vstack(weighted_score_list)
 	weighted_score_pd = pd.DataFrame(data=weighted_score_list, index=channel_list_vis, columns=methods_vis)
 	colors = ['deepskyblue', 'darkred', 'darkgoldenrod']
 	
 	weighted_score_pd.plot(kind='barh', color=colors)
-
-	plt.legend( loc='upper center', bbox_to_anchor=(0.4, -0.08), ncol=3)
+	plt.xlabel('Quality Score')
+	plt.legend(loc='upper center', bbox_to_anchor=(0.4, -0.1), ncol=3)
 	plt.tight_layout()
-	
-	# Show the plot
-	plt.savefig(f'{os.path.dirname(pca_dir)}/fig/AICS_original_scores.png', dpi=500)
-	
+	plt.savefig(f'{os.path.dirname(pca_dir)}/fig/ACSS_original_scores.png', dpi=500)
--- plotting/Supp_figure_1.py
+++ plotting/Supp_figure_1.py
@@ -1,20 +1,16 @@
 import pickle
 import bz2
 from skimage.io import imsave
+import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors
-import numpy as np
 
 JI_threshold = 0.2
 IMCpath = './data/IMC_3D/florida-3d-imc/a296c763352828159f3adfa495becf3e'
 IMAGEpath = IMCpath
 
 mask_2D_XY_stack = pickle.load(bz2.BZ2File(f'{IMAGEpath}/original/mask_aics_classic_matched_3D_final_0.0.pkl', 'rb'))
-
-mask_2D_XY_stack[mask_2D_XY_stack>0] = 1
-imsave(f'{IMAGEpath}/original/mask_aics_classic_matched_3D_final_0.2.tif', mask_2D_XY_stack)
-np.load('./data/IMC_3D/florida-3d-imc/a296c763352828159f3adfa495becf3e/original/metrics/metrics_aics_classic_0.0.npy')
-np.load('./data/IMC_3D/florida-3d-imc/a296c763352828159f3adfa495becf3e/original/metrics/metrics_aics_classic_0.2.npy')
+mask_2D_XY_stack[mask_2D_XY_stack > 0] = 1
 
 mask_2D_XY_stack = pickle.load(bz2.BZ2File(f'{IMAGEpath}/original/mask_deepcell_membrane-0.12.6_matched_stack_XY_{JI_threshold}.pkl', 'rb'))
 mask_2D_XZ_stack = pickle.load(bz2.BZ2File(f'{IMAGEpath}/original/mask_deepcell_membrane-0.12.6_matched_stack_XZ_{JI_threshold}.pkl', 'rb'))
@@ -24,105 +20,84 @@ mask_2D_YZ_stack = np.rot90(mask_2D_YZ_stack, k=1, axes=(0, 1))
 mask_no_repair = pickle.load(bz2.BZ2File(f'{IMAGEpath}/original/mask_deepcell_membrane-0.12.6_nonrepaired_3D_{JI_threshold}.pkl', 'rb'))
 mask_repair = pickle.load(bz2.BZ2File(f'{IMAGEpath}/original/mask_deepcell_membrane-0.12.6_matched_3D_{JI_threshold}.pkl', 'rb'))
 
-
 def create_slice_fig_new(volume, slice_type, index=None, repaired_coords=None, fragment_idx=None):
-	
-	if repaired_coords != None:
-		volume_copy = np.zeros_like(volume)
-		volume_copy[repaired_coords] = volume[repaired_coords]
-		volume = volume_copy
-	XZ = volume[:, index[0], :]
-	YZ = volume[:, :, index[1]]
+    if repaired_coords is not None:
+        volume_copy = np.zeros_like(volume)
+        volume_copy[repaired_coords] = volume[repaired_coords]
+        volume = volume_copy
+    XZ = volume[:, index[0], :]
+    YZ = volume[:, :, index[1]]
 
+    def reindex_and_convert(array):
+        if len(np.unique(array)) > 2:
+            unique_values = np.unique(array[array > 0])
+            reindex_map = {0: 0, fragment_idx: 2}
+            reindex_map.update({value: i + 1 for i, value in enumerate(unique_values) if value != fragment_idx})
+            result = np.vectorize(lambda x: reindex_map.get(x, 0), otypes=[int])(array)
+            return result
+        else:
+            return np.where(array > 0, 1, 0).astype(int)
 
-	# Function to reindex and convert to integer type.
-	def reindex_and_convert(array):
-		if len(np.unique(array)) > 2:
-			unique_values = np.unique(array[array > 0])
-			# Add mappings for non-zero values.
-			reindex_map = {0: 0, fragment_idx: 20}
-			# Start mapping other unique values from 2 onwards.
-			reindex_map.update({value: i + 2 for i, value in enumerate(unique_values) if value != fragment_idx})
-			# Use reindex_map.get(value, 0) to handle unexpected values gracefully.
-			result = np.vectorize(lambda x: reindex_map.get(x, 0), otypes=[int])(array)
-			# print(reindex_map)
-			return result
-		else:
-			return np.where(array > 0, 1, 0).astype(int)
+    binary_XZ = reindex_and_convert(XZ).astype(np.uint8)
+    binary_YZ = reindex_and_convert(YZ).astype(np.uint8)
 
-	binary_XZ = reindex_and_convert(XZ).astype(np.uint8)
-	binary_YZ = reindex_and_convert(YZ).astype(np.uint8)
-	
-	base_colors = plt.cm.tab20.colors[2:]  # Adjusted to exclude similar colors if needed
+    custom_colors = ['white', 'darkblue', 'cyan']
+    cmap = matplotlib.colors.ListedColormap(custom_colors)
 
-	# Define a custom color mapping: white for 0, cyan for 1, then others
-	custom_colors = ['white', 'cyan'] + list(base_colors)
+    plt.rcParams['figure.facecolor'] = 'white'
+    plt.rcParams['axes.facecolor'] = 'white'
+    plt.rcParams['savefig.facecolor'] = 'white'
 
-	# Create a new colormap from the custom colors
-	cmap = matplotlib.colors.ListedColormap(custom_colors)
-	
-	# cmap = plt.get_cmap('tab20')
-	
-	plt.rcParams['figure.facecolor'] = 'white'
-	plt.rcParams['axes.facecolor'] = 'white'
-	plt.rcParams['savefig.facecolor'] = 'white'
+    aspect_ratio_XZ = binary_XZ.shape[1] / binary_XZ.shape[0]
+    aspect_ratio_YZ = binary_YZ.shape[1] / binary_YZ.shape[0]
 
-	aspect_ratio_XZ = binary_XZ.shape[1] / binary_XZ.shape[0]
-	aspect_ratio_YZ = binary_YZ.shape[1] / binary_YZ.shape[0]
+    base_height = 4
+    width_XZ = base_height * aspect_ratio_XZ
+    width_YZ = base_height * aspect_ratio_YZ
+    fig_width = max(width_XZ, width_YZ)
 
-	base_height = 4
-	width_XZ = base_height * aspect_ratio_XZ
-	width_YZ = base_height * aspect_ratio_YZ
-	fig_width = max(width_XZ, width_YZ)
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(fig_width, 2 * base_height))
+    ax1.imshow(binary_XZ, cmap=cmap, origin='lower', aspect='equal')
+    ax1.set_title('XZ View', fontsize=40)
+    ax1.axis('off')
 
-	fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(fig_width, 2 * base_height))
-	ax1.imshow(binary_XZ, cmap=cmap, origin='lower', aspect='equal')
-	ax1.set_title('XZ View', fontsize=40)
-	ax1.axis('off')
+    ax2.imshow(binary_YZ, cmap=cmap, origin='lower', aspect='equal')
+    ax2.set_title('YZ View', fontsize=40)
+    ax2.axis('off')
 
-	ax2.imshow(binary_YZ, cmap=cmap, origin='lower', aspect='equal')
-	ax2.set_title('YZ View', fontsize=40)
-	ax2.axis('off')
-	
-	
-	plt.tight_layout()
-	plt.savefig(f'./fig/supp_fig_1_2D_{slice_type}.png', dpi=300)
-
+    plt.tight_layout()
+    plt.savefig(f'/home/hrchen/Documents/Research/hubmap/script/2D-3D/fig/supp_fig_1_2D_{slice_type}.png', dpi=300)
 
 def isolate_cell(segmentation, cell_index):
-	isolated_cell = np.zeros_like(segmentation)
-	isolated_cell[segmentation == cell_index] = cell_index
-	cell_coords = np.where(isolated_cell == cell_index)
-	isolated_cell = isolated_cell[min(cell_coords[0]):max(cell_coords[0])+1,
-					min(cell_coords[1]):max(cell_coords[1])+1,
-					min(cell_coords[2]):max(cell_coords[2])+1]
-	# return isolated_cell, np.array((int(np.mean(cell_coords[0])), int(np.mean(cell_coords[1])), int(np.mean(cell_coords[2]))))
-	return isolated_cell, cell_coords
+    isolated_cell = np.zeros_like(segmentation)
+    isolated_cell[segmentation == cell_index] = cell_index
+    cell_coords = np.where(isolated_cell == cell_index)
+    isolated_cell = isolated_cell[min(cell_coords[0]):max(cell_coords[0])+1,
+                                  min(cell_coords[1]):max(cell_coords[1])+1,
+                                  min(cell_coords[2]):max(cell_coords[2])+1]
+    return isolated_cell, cell_coords
 
 def crop_mask(mask, bounding_box):
-	cropped_mask = mask[bounding_box[0]:bounding_box[1], bounding_box[2]:bounding_box[3], bounding_box[4]:bounding_box[5]]
-	return cropped_mask
+    cropped_mask = mask[bounding_box[0]:bounding_box[1], bounding_box[2]:bounding_box[3], bounding_box[4]:bounding_box[5]]
+    return cropped_mask
 
 Z = 20
 X = 164
 Y = 353
-vis_coords = [9,14]
+vis_coords = [9, 14]
 
-
-
-isolated_cell_no_repair, cell_coords_no_repair = isolate_cell(mask_no_repair, mask_no_repair[Z,Y,X])
-isolated_cell_mask_XY, cell_coords_XY = isolate_cell(mask_2D_XY_stack, mask_2D_XY_stack[Z,Y,X])
-isolated_cell_mask_XZ, cell_coords_XZ = isolate_cell(mask_2D_XZ_stack, mask_2D_XZ_stack[Z,Y,X])
-isolated_cell_mask_YZ, cell_coords_YZ = isolate_cell(mask_2D_YZ_stack, mask_2D_YZ_stack[Z,Y,X])
-isolated_cell_repair, cell_coords_repair = isolate_cell(mask_repair, mask_repair[Z,Y,X])
+isolated_cell_no_repair, cell_coords_no_repair = isolate_cell(mask_no_repair, mask_no_repair[Z, Y, X])
+isolated_cell_mask_XY, cell_coords_XY = isolate_cell(mask_2D_XY_stack, mask_2D_XY_stack[Z, Y, X])
+isolated_cell_mask_XZ, cell_coords_XZ = isolate_cell(mask_2D_XZ_stack, mask_2D_XZ_stack[Z, Y, X])
+isolated_cell_mask_YZ, cell_coords_YZ = isolate_cell(mask_2D_YZ_stack, mask_2D_YZ_stack[Z, Y, X])
+isolated_cell_repair, cell_coords_repair = isolate_cell(mask_repair, mask_repair[Z, Y, X])
 
 Z_min = min(cell_coords_XZ[0])
-Z_max = max(cell_coords_XZ[0])+1
+Z_max = max(cell_coords_XZ[0]) + 1
 Y_min = min(cell_coords_XY[1])
-Y_max = max(cell_coords_XZ[1])+1
+Y_max = max(cell_coords_XZ[1]) + 1
 X_min = min(cell_coords_XZ[2])
-X_max = max(cell_coords_XZ[2])+1
-
+X_max = max(cell_coords_XZ[2]) + 1
 
 bounding_box = (Z_min, Z_max, Y_min, Y_max, X_min, X_max)
 cropped_cell_mask_XY = crop_mask(mask_2D_XY_stack, bounding_box)
@@ -131,18 +106,24 @@ cropped_cell_mask_YZ = crop_mask(mask_2D_YZ_stack, bounding_box)
 cropped_cell_mask_no_repair = crop_mask(mask_no_repair, bounding_box)
 cropped_cell_mask_repair = crop_mask(mask_repair, bounding_box)
 
-cropped_cell_mask_XY[np.where(cropped_cell_mask_XY != mask_2D_XY_stack[Z,Y,X])] = 0
-cropped_cell_mask_XZ[np.where(cropped_cell_mask_XZ != mask_2D_XZ_stack[Z,Y,X])] = 0
-cropped_cell_mask_YZ[np.where(cropped_cell_mask_YZ != mask_2D_YZ_stack[Z,Y,X])] = 0
-# cropped_cell_mask_no_repair[np.where(cropped_cell_mask_no_repair != mask_no_repair[Z,Y,X])] = 0
-cropped_cell_mask_repair[np.where(cropped_cell_mask_repair != mask_repair[Z,Y,X])] = 0
+cropped_cell_mask_XY[np.where(cropped_cell_mask_XY != mask_2D_XY_stack[Z, Y, X])] = 0
+cropped_cell_mask_XZ[np.where(cropped_cell_mask_XZ != mask_2D_XZ_stack[Z, Y, X])] = 0
+cropped_cell_mask_YZ[np.where(cropped_cell_mask_YZ != mask_2D_YZ_stack[Z, Y, X])] = 0
+cropped_cell_mask_no_repair[np.where(cropped_cell_mask_no_repair != mask_no_repair[Z, Y, X])] = 0
+cropped_cell_mask_repair[np.where(cropped_cell_mask_repair != mask_repair[Z, Y, X])] = 0
+
+dual_color = True
+if dual_color:
+    color_idx = 2
+    mask_no_repair_coords = np.where(cropped_cell_mask_no_repair != 0)
+    cropped_cell_mask_XY[mask_no_repair_coords] = color_idx
+    cropped_cell_mask_XZ[mask_no_repair_coords] = color_idx
+    cropped_cell_mask_YZ[mask_no_repair_coords] = color_idx
+    cropped_cell_mask_repair[mask_no_repair_coords] = color_idx
 
 repaired_coords = np.where(cropped_cell_mask_repair != 0)
-create_slice_fig_new(cropped_cell_mask_no_repair, 'fragment', vis_coords, repaired_coords, mask_repair[Z,Y,X])
+create_slice_fig_new(cropped_cell_mask_no_repair, 'fragment', vis_coords, repaired_coords, mask_repair[Z, Y, X])
 create_slice_fig_new(cropped_cell_mask_repair, 'repaired', vis_coords)
 create_slice_fig_new(cropped_cell_mask_XY, 'Z_stack', vis_coords)
 create_slice_fig_new(cropped_cell_mask_XZ, 'Y_stack', vis_coords)
 create_slice_fig_new(cropped_cell_mask_YZ, 'X_stack', vis_coords)
-
-
-
